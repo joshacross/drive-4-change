68c480629d6bae871620d58bddeab5ebdda33e85
joshacross <joshuaalexandercross@gmail.com>
2022-06-05 09:20:42 -0500

    setup globalState for remaining pages and components

--- client/src/utils/globalstate.js
+++ client/src/utils/globalstate.js
@@ -9,3 +9,33 @@ export const showDashboardState = atom({
     key: 'dashboardState',
     default: false
 });
+
+export const signupState = atom({
+    key: 'signUpState',
+    default: false
+});
+
+export const notificationState = atom({
+    key: 'notificationState',
+    default: false
+});
+
+export const diagnosticState = atom({
+    key: 'diagnosticState', 
+    default: false
+});
+
+export const showTripsState = atom({
+    key: 'showTripsState',
+    default: false
+})
+
+export const showRewardsState = atom({
+    key: 'showRewardsState', 
+    default: false
+});
+
+export const showPointsState = atom({
+    key: 'showPointsState', 
+    default: false
+})
