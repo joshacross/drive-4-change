12e4944c4eaf5dc92e9fd7b4a58723895fc1308d
joshacross <joshuaalexandercross@gmail.com>
2022-06-04 22:09:22 -0500

    updated notifications component

--- client/src/components/Notifications/index.js
+++ client/src/components/Notifications/index.js
@@ -8,44 +8,19 @@ import TableRow from '@mui/material/TableRow';
 import Title from '../Title';
 
 // Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-  return { id, date, name, shipTo, paymentMethod, amount };
+function createData(id, date, notification, trip, monitorType, points) {
+  return { id, date, notification, trip, monitorType, points };
 }
 
 const rows = [
   createData(
     0,
     '16 Mar, 2019',
-    'Elvis Presley',
-    'Tupelo, MS',
-    'VISA ⠀•••• 3719',
-    312.44,
-  ),
-  createData(
-    1,
-    '16 Mar, 2019',
-    'Paul McCartney',
-    'London, UK',
-    'VISA ⠀•••• 2574',
-    866.99,
-  ),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-  createData(
-    3,
-    '16 Mar, 2019',
-    'Michael Jackson',
-    'Gary, IN',
-    'AMEX ⠀•••• 2000',
-    654.39,
-  ),
-  createData(
-    4,
-    '15 Mar, 2019',
-    'Bruce Springsteen',
-    'Long Branch, NJ',
-    'VISA ⠀•••• 5919',
-    212.79,
-  ),
+    'Congrats! You Earned Points!',
+    123,
+    'gas consumption',
+    444,
+  )
 ];
 
 function preventDefault(event) {
@@ -60,20 +35,20 @@ export default function Notifications() {
         <TableHead>
           <TableRow>
             <TableCell>Date</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
+            <TableCell>Notification</TableCell>
+            <TableCell>Trip</TableCell>
+            <TableCell>For</TableCell>
+            <TableCell align="right">Points</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.id}>
               <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{`$${row.amount}`}</TableCell>
+              <TableCell>{row.notification}</TableCell>
+              <TableCell>Trip #{row.trip}</TableCell>
+              <TableCell>{row.monitorType}</TableCell>
+              <TableCell align="right">{`${row.points}`}</TableCell>
             </TableRow>
           ))}
         </TableBody>
