c3ca550c4db445bae910ec58b96901a02d608f6a
joshacross <joshuaalexandercross@gmail.com>
2022-06-05 11:36:26 -0500

    updated authentication and received bearer token response

--- client/src/components/Rewards/index.js
+++ client/src/components/Rewards/index.js
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import Typography from '@mui/material/Typography';
 
 
 function Rewards() {
 
   return (
     <React.Fragment>
+      <Typography component="h2" variant="h6" color="primary" gutterBottom>
+      Rewards Tier: Level 5
+    </Typography>
       <h2>Rewards Section</h2>
     </React.Fragment>
   );
